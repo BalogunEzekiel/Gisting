64bb93acf365cb4f4091a302505c046be83174c4
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-08-07 18:28:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -34,8 +34,8 @@ st.markdown("💡 Speak clearly into your microphone...")
 rtc_configuration = RTCConfiguration(
     {
         "iceServers": [
+            {"urls": ["stun:stun2.l.google.com:19302"]},
             {"urls": ["stun:stun.ekiga.net"]},
-            {"urls": ["stun:stun2.l.google.com:19302"]},            
             {
                 "urls": ["turn:openrelay.metered.ca:80", "turn:openrelay.metered.ca:443"],
                 "username": "openrelayproject",
